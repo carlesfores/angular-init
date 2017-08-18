8cdfcb0aa0b43a78ffee658d6a5059b5ae14e008
Carles Forès <carles.fores@basetis.com>
2017-08-18 21:39:57 +0200

    gulp task

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            "error",
+            {
+                "args": "none"
+            }
+        ]
+    },
+    "globals": {
+        "angular": true,
+        "module": true,
+        "require": true,
+        "process": true
+    }
+};
---
+++ gulpfile.js
@@ -0,0 +1,122 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const babel = require('babelify');
+const copy = require('gulp-copy');
+const jslint = require('gulp-jslint');
+const eslint = require('gulp-eslint');
+const jasmine = require('gulp-jasmine');
+const Server = require('karma').Server;
+const jade = require('gulp-jade');
+const sass = require('gulp-sass');
+const serve = require('gulp-serve');
+const sourceFiles = ['./src/**/*.*', '!./src/**/*.es', '!./src/**/*.pug'];
+
+// define functions
+function compile () {
+    'use strict';
+    let bundler = watchify(browserify('./src/index.js', {debug: true}).transform(babel.configure({
+        presets: ['es2015']
+    })));
+    function rebundle () {
+        return bundler.bundle()
+            .on('error', function (err) {
+                console.error(err);
+                this.emit('end');
+            })
+            .pipe(source('build.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./build'));
+    }
+    return rebundle();
+}
+
+function copyStatics () {
+    'use strict';
+    return gulp.src(sourceFiles)
+        .pipe(copy('build', {prefix: 1}));
+}
+
+function sassTask() {
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function templates() {
+    'use strict';
+    let YOUR_LOCALS = {};
+    gulp.src('./src/**/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('./build/'))
+}
+
+function assets() {
+    'use strict';
+    gulp.src('./src/assets/*.*')
+        .pipe(gulp.dest('./build/assets'))
+}
+
+function esLint () {
+    return gulp.src(['./src/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+}
+
+// define gup task
+gulp.task('serve', serve('build'));
+
+gulp.task('td', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js'
+    }, done).start();
+});
+
+gulp.task('test', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+gulp.task('buildSimple', ['esLint'],() => {
+    return compile();
+});
+
+gulp.task('copyStatics', () => {
+    return copyStatics();
+});
+
+gulp.task('templates', () => {
+    return templates();
+});
+
+gulp.task('sass', function () {
+    return sassTask();
+});
+
+gulp.task('assets', function () {
+    return assets();
+});
+
+gulp.task('esLint', function () {
+    return esLint();
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.js', ['buildSimple']);
+    gulp.watch('./src/styles/**/*.scss', ['sass']);
+    gulp.watch('./src/**/*.jade', ['templates']);
+});
+
+gulp.task('default', ['buildSimple', 'assets', 'copyStatics', 'templates', 'sass', 'serve', 'watch']);
+
+gulp.task('build', ['buildSimple', 'copyStatics', 'templates', 'sass']);
